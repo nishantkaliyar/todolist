40c93c702cd62f94cafa97e010639b10e40c238b
nishantkaliyar <nishantkaliyar@gmail.com>
2026-09-18 15:35:55 +0530

    morebugs fixed

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "https://todolist-rho-ivory.vercel.app/",
+  origin: process.env.FRONTEND_URL,
   credentials: true
 }))
 app.use(express.json())
